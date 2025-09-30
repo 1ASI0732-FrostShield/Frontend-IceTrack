fb7d3c631feae615a54305ca231521660beede91
Pedro Nanfuñay <pedro18nan@gmail.com>
2025-09-30 18:52:21 -0500

    chore: initial commit.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import i18n from "@/i18n.js";
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material'
+import router from "@/router.js";
+
+createApp(App)
+    .use(i18n)
+    .use(PrimeVue , { theme: { preset: Material}, ripple: true})
+    .use(router)
+    .mount('#app')
