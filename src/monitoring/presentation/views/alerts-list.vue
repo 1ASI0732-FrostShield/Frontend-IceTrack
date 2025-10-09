a00c249bff3de3b484471d8b876c7c9e3f496d2c
julio645 <julio030807@gmail.com>
2025-10-08 21:05:21 -0500

    Update alerts-list.vue

--- src/monitoring/presentation/views/alerts-list.vue
+++ src/monitoring/presentation/views/alerts-list.vue
@@ -1,16 +1,16 @@
 <script setup>
+
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
+
 </script>
 
 <template>
   <section>
-    <h1 class="text-2xl font-semibold mb-4">{{ t('alerts.list.title') }}</h1>
+    <h1 class="text-2xl font-semibold mb-2">
+      {{ t('alerts.list.title') }}
+    </h1>
 
-    <div class="flex gap-2">
-      <RouterLink :to="{ name: 'equipment-detail', params: { equipmentId: '1' } }">
-        <pv-button :label="t('equipments.detail.title')" />
-      </RouterLink>
-    </div>
+    <p>Aquí se mostrarán las alertas del equipo.</p>
   </section>
 </template>
