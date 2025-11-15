7445b2d5647ef29b846e9f08d3caca5cb159513f
Pedro Nanfuñay <pedro18nan@gmail.com>
2025-11-14 19:50:23 -0500

    feat(reporting): add report form for create a report.

--- src/reporting/presentation/reporting-routes.js
+++ src/reporting/presentation/reporting-routes.js
@@ -1,9 +1,11 @@
 const reportList = () => import('./views/reports-list.vue');
 const reportDetail = () => import('./views/report-detail.vue');
+const reportNew = () => import('./views/report-form.vue');
 
 const reportingRoutes = [
     {   path: 'reports',             name: 'reporting-report',      component: reportList, meta: {titleKey: 'reports.list.title'}},
     {   path: 'reports/:id',         name: 'reporting-report-detail',    component: reportDetail, meta: {titleKey: 'reports.list.detail.title'}},
+    {   path: 'reports/new',         name: 'reporting-report-new',    component: reportNew, meta: {titleKey: 'reports.form.title'}}
 ];
 
 export default reportingRoutes;
