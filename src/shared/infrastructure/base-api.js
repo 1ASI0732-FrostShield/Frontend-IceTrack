354c57e28821c01ec7633d791ee5493c871f1750
Kenyi15upc <u202220138@upc.edu.pe>
2025-12-02 17:26:40 -0500

    feat(base-api): add auth token handling via request interceptor for secured API requests

--- src/shared/infrastructure/base-api.js
+++ src/shared/infrastructure/base-api.js
@@ -1,13 +1,32 @@
 import axios from "axios";
+import { useAuthStore } from "@/iam/application/auth.store.js";
 
 const platformApi = import.meta.env.VITE_API_BASE_URL;
+
 export class BaseApi {
     #http;
+
     constructor() {
         this.#http = axios.create({
             baseURL: platformApi,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        this.#http.interceptors.request.use(config => {
+            const authStore = useAuthStore();
+            const token = authStore.token;
+
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            return config;
+        }, error => {
+            return Promise.reject(error);
         });
     }
+
     get http() {
         return this.#http;
     }
