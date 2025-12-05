6b5094da56d5780e5d77dd7e5fc032de8ad17ef2
julio645 <julio030807@gmail.com>
2025-12-05 16:31:53 -0500

    Update assets-management.store.js

--- src/assets-management/application/assets-management.store.js
+++ src/assets-management/application/assets-management.store.js
@@ -5,11 +5,31 @@ import { AssetsManagementApi } from "@/assets-management/infrastructure/assets-m
 
 const assetsManagementApi = new AssetsManagementApi();
 
+/**
+ * Store for Assets Management context.
+ */
 const useAssetsManagementStore = defineStore('assetsManagement', () => {
+    /**
+     * List of site entities.
+     * @type {import('vue').Ref<Site[]>}
+     */
     const sites = ref([]);
+    /**
+     * List of errors encountered during API operations.
+     * @type {import('vue').Ref<Error[]>}
+     */
     const errors = ref([]);
+    /**
+     * Whether sites have been loaded from the API.
+     * @type {import('vue').Ref<boolean>}
+     */
     const sitesLoaded = ref(false);
 
+    /**
+     * Fetches sites from the API and updates state.
+     * @function
+     * @returns {void}
+     */
     function fetchSites() {
         assetsManagementApi.getSites().then(response => {
             sites.value = SitesAssembler.toEntitiesFromResponse(response);
@@ -28,5 +48,6 @@ const useAssetsManagementStore = defineStore('assetsManagement', () => {
         fetchSites,
     }
 });
+
 
 export default useAssetsManagementStore;
